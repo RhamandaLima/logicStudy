d77b67e1defa3d97baf25b31976326d86cbf7e6b
Rhamanda Lima <cathyannarhamanda@gmail.com>
2021-06-23 21:23:59 -0300

    Add function

--- Programming Logic II - Classic Games/sketch.js
+++ Programming Logic II - Classic Games/sketch.js
@@ -1,7 +1,10 @@
+//variáveis da bolinha
 let xBolinha = 300;
 let yBolinha = 200; 
 let diametro = 15;
+let raio = diametro / 2;
 
+//velocidade da bolinha
 let velocidadeXBolinha = 6;
 let velocidadeYBolinha = 6; 
 
@@ -11,18 +14,29 @@ function setup() {
 
 function draw() {
   background(0);
+  mostraBolinha();
+  movimentaBolinha();
+  verificaColisaoBorda();
+  
+}
+
+function mostraBolinha(){
   circle(xBolinha, yBolinha, diametro);
+}
+
+function movimentaBolinha(){
   xBolinha += velocidadeXBolinha;
   yBolinha += velocidadeYBolinha;
-  
-  if(xBolinha > width || 
-    xBolinha < 0){
+}
+
+function verificaColisaoBorda(){
+  if(xBolinha + raio > width || 
+    xBolinha - raio < 0){
     velocidadeXBolinha *= -1;
     
   }
   
-  if (yBolinha > height || yBolinha < 0){
+  if (yBolinha + raio > height || yBolinha - raio < 0){
     velocidadeYBolinha *= -1;
   }
-  
 }
