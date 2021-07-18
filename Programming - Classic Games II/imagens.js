177cca323f41e15521f9151fdc6da494ff9ef20c
Rhamanda Lima <cathyannarhamanda@gmail.com>
2021-07-18 00:12:55 -0300

    Add other cars

--- Programming - Classic Games II/imagens.js
+++ Programming - Classic Games II/imagens.js
@@ -3,9 +3,13 @@
 let imagemDaEstrada; 
 let imagemDoAtor;
 let imagemDoCarro;
+let imagemDoCarro2;
+let imagemDoCarro3;
 
 function preload(){
   imagemDaEstrada = loadImage("Imagens/estrada.png");
   imagemDoAtor = loadImage("Imagens/ator-1.png");
   imagemDoCarro = loadImage("Imagens/carro-1.png");
+  imagemDoCarro2 = loadImage("Imagens/carro-2.png");
+  imagemDoCarro3 = loadImage("Imagens/carro-3.png");
 }
