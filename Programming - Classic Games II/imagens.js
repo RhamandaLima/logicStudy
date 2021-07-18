9d5ca9e128f5052c5264f6a6488f68dee57aa317
Rhamanda Lima <cathyannarhamanda@gmail.com>
2021-07-17 23:59:38 -0300

    Add divisão do codigo

---
+++ Programming - Classic Games II/imagens.js
@@ -0,0 +1,11 @@
+//codigo da imagem
+
+let imagemDaEstrada; 
+let imagemDoAtor;
+let imagemDoCarro;
+
+function preload(){
+  imagemDaEstrada = loadImage("Imagens/estrada.png");
+  imagemDoAtor = loadImage("Imagens/ator-1.png");
+  imagemDoCarro = loadImage("Imagens/carro-1.png");
+}
