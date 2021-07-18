aeaf87dcdc9335f2f692b34992bd5c066ca63189
Rhamanda Lima <cathyannarhamanda@gmail.com>
2021-07-18 02:03:54 -0300

    Add point

--- Programming - Classic Games II/ator.js
+++ Programming - Classic Games II/ator.js
@@ -1,8 +1,10 @@
 //codigo do ator
 
-let xAtor = 100; 
+let xAtor = 85; 
 let yAtor = 366;
 let colisao = false;
+let meusPontos = 0; 
+
 
 function mostraAtor(){
   image(imagemDoAtor, xAtor, yAtor, 30, 30);
@@ -20,16 +22,30 @@ function movimentaAtor(){
 function verificaColisao(){
   //collideRectCircle(x1, y1, width1, height1, cx, cy, diameter)
   
-  for(let i = 0; i < imagemCarros.length; i = i + 1) {
+  for(let i = 0; i < imagemCarros.length; i++) {
     
     colisao = collideRectCircle(xCarros[i], yCarros[i], comprimentoCarro, alturaCarro, xAtor, yAtor, 15);
     
     if(colisao){
-      colidiu()
+      voltaAtorParaPosicaoInicial();
     }
   }
 }
 
-function colidiu(){
+function voltaAtorParaPosicaoInicial(){
   yAtor = 366;
+}
+
+function incluiPontos(){
+  textAlign(CENTER);
+  textSize(25);
+  fill(color(0,255,154));
+  text(meusPontos, width/5, 26.5);
+}
+
+function marcaPonto(){
+  if(yAtor <= 15){
+    meusPontos += 1;
+    voltaAtorParaPosicaoInicial();
+  }
 }
